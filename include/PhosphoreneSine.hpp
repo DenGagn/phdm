d9824bfb6180d5d0440168697ccc1e28fdae1ac1
DenGagn <denisg6@hotmail.com>
2018-06-14 14:46:38 -0400

    Initial import of PhosphoreneSine.cpp

---
+++ include/PhosphoreneSine.hpp
@@ -0,0 +1,136 @@
+#pragma once
+
+#include <armadillo>
+
+typedef arma::Col<arma::cx_double> state_type; // Type of container used to hold the state vector
+
+/*!
+* \class base_sine
+*
+* \brief Base class for a few-cycle sine wave with variable duration and
+* amplitude, but zero carrier-envelope phase (version for phosphorene)
+*
+* \author Author: D. Gagnon <denisg6@hotmail.com>
+*/
+class base_sine {
+
+    // Parameters of the Hamiltonian
+    double m_kx,m_ky,m_omega,m_a,m_E0,m_ellip;
+
+private:
+
+    // Physical constants definitions
+    double hbar = 6.5821195140e-16; // Planck constant over 2 pi in eV s
+
+public:
+
+    /// Constructor taking as input the parameters of the Hamiltonian
+    /// @param kx float, x-component of the momentum
+    /// @param ky float, y-component of the momentum
+    /// @param omega float, angular frequency of the field in rad/s
+    /// @param a float, envelope frequency (normalized)
+    /// @param E0 float, electric field peak value in V/m
+    /// @param ellip float, ellipticity of the field in the circularly polarized case
+    base_sine(double kx, double ky, double omega, double a, double E0, double ellip=0.0 )
+        : m_kx(kx)
+        , m_ky(ky)
+        , m_omega(omega)
+        , m_a(a)
+        , m_E0(E0)
+        , m_ellip(ellip) { }
+
+    /// Real part of gamma factor appearing in the Hamiltonian
+    virtual std::complex<double> Gamma0 (double, double) = 0;
+
+    /// Imaginary part of gamma factor appearing in the Hamiltonian
+    virtual std::complex<double> Gamma1 (double, double) = 0;
+
+    /// Overload of operator() for ODE integration
+    void operator() ( const state_type &z, state_type &dzdt, const double t)
+    {
+
+        // Conversion factor
+        double factor = m_E0/(hbar*m_omega);
+
+        // Peierls substitution
+        double kx_field = m_kx + factor*Gx(t);
+        double ky_field = m_ky + m_ellip*factor*Gy(t);
+
+        // Compute gamma factors
+        auto Gamma0_field = Gamma0(kx_field, ky_field);
+        auto Gamma1_field = Gamma1(kx_field, ky_field);
+
+        // The ODE system
+        dzdt[0] = -1j*(1.0/hbar)*(Gamma0_field*z[0] + Gamma1_field*z[1]);
+        dzdt[1] = -1j*(1.0/hbar)*(std::conj(Gamma1_field)*z[0] + Gamma0_field*z[1]);
+
+    }
+
+    /// Normalized vector potential, x-component
+    double Gx (double t)
+    {
+
+        return 0.25*m_omega*(sin((2.0*m_a-m_omega)*t)/(2.0*m_a-m_omega)
+                             + sin((2.0*m_a+m_omega)*t)/(2.0*m_a+m_omega)
+                             - 2.0*sin(m_omega*t)/m_omega);
+
+//         // Version used in 10.1364/JOSAB.35.000958
+//         return sin(m_omega*t)*sin(m_a*t)*sin(m_a*t);
+    }
+
+    /// Normalized vector potential, y-component
+    double Gy (double t)
+    {
+//         return cos(m_omega*t)*sin(m_a*t)*sin(m_a*t);
+        return 0.0;
+    }
+
+};
+
+/*!
+* \class tight_binding_sine
+*
+* \brief Tight-binding model for phosphorene (RHS of ODE system)
+*
+* The excitation
+* is a few-cycle sine wave with variable duration and amplitude, but zero
+* carrier-envelope phase
+*
+*/
+class tight_binding_sine : public base_sine {
+
+private:
+    // Tight-binding constant definitions (see http://arxiv.org/abs/1710.05808)
+    double eta = 0.58; // eV A^2
+    double nu  = 1.01; // eV A^2
+    double gx =  3.93; // eV A^2
+    double gy =  3.83; // eV A^2
+    double d0 = -0.42; // eV
+    double d1 =  0.76; // eV
+    double chi = 5.25; // eV A
+
+public:
+    /// Constructor taking as input the parameters of the  Hamiltonian (tight-binding specialization)
+    /// @param kx float, x-component of the momentum
+    /// @param ky float, y-component of the momentum
+    /// @param omega float, angular frequency of the field in rad/s
+    /// @param a float, envelope frequency (normalized)
+    /// @param E0 float, electric field peak value in V/m
+    /// @param ellip float, ellipticity of the field in the circularly polarized case
+    tight_binding_sine(double kx, double ky, double omega, double a, double E0, double ellip=0.0 ) :
+        base_sine(kx, ky, omega, a, E0, ellip ) {}
+
+    /// Gamma0 factor appearing in the tight-binding Hamiltonian
+    std::complex<double> Gamma0(double kx, double ky)
+    {
+        return d0 + eta*kx*kx + nu*ky*ky;
+    }
+
+    /// Gamma1 factor appearing in the tight-binding Hamiltonian
+    std::complex<double> Gamma1(double kx, double ky)
+    {
+        return d1 + 1j*chi*ky + gx*kx*kx + gy*ky*ky;
+    }
+
+
+};
