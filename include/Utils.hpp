07daad26420c9ddcfa3876072f4ed98ebd85d8a2
DenGagn <denisg6@hotmail.com>
2018-06-15 11:46:28 -0400

    Added generalized function EigenStateGeneral to Utils.hpp

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -20,26 +20,86 @@ typedef std::vector< double > state_type; // Type of container used to hold the 
 template <typename T>
 state_type EigenState(T model, double kx, double ky, double sign=1.0)
 {
-        // Prepare initial state (calculate gamma factor and its angles)
-        double Re_Gamma = model.Re_Gamma(kx, ky);
-        double Im_Gamma = model.Im_Gamma(kx, ky);
-        double angle_Gamma = atan2(Im_Gamma,Re_Gamma);
+    // Prepare initial state (calculate gamma factor and its angles)
+    double Re_Gamma = model.Re_Gamma(kx, ky);
+    double Im_Gamma = model.Im_Gamma(kx, ky);
+    double angle_Gamma = atan2(Im_Gamma,Re_Gamma);
 
-        state_type eigen = {0.0,0.0,0.0,0.0};
+    state_type eigen = {0.0,0.0,0.0,0.0};
 
-        // Assign values of initial state (the ket)
-        eigen[0] = sqrt(0.5);
-        // eigen[1] = 0.0;
-        eigen[2] = sign*sqrt(0.5)*(cos(angle_Gamma));
-        eigen[3] = sign*sqrt(0.5)*(sin(angle_Gamma));
-        
-        return eigen;
+    // Assign values of initial state (the ket)
+    eigen[0] = sqrt(0.5);
+    // eigen[1] = 0.0;
+    eigen[2] = sign*sqrt(0.5)*(cos(angle_Gamma));
+    eigen[3] = sign*sqrt(0.5)*(sin(angle_Gamma));
+
+    return eigen;
+}
+
+/// Function computing an eigenstate for a given model
+/// with a complex 2 x 2 Hamiltonian
+/// @param model T, the Hamiltonian model
+/// @param kx float, x-component of the momentum
+/// @param ky float, y-component of the momentum
+/// @param bool pos, true if positive eigenstate, false otherwise
+template <typename T>
+state_type EigenStateGeneral(T model, double kx, double ky, bool pos=true)
+{
+    // Calculate matrix elements of the Hamiltonian
+    std::complex<double> Gamma0 = model.Gamma0(kx,ky);
+    std::complex<double> Gamma1 = model.Gamma1(kx,ky);
+
+    // Define Hamiltonian matrix
+    arma::cx_mat Ham;
+    Ham.resize(2,2);
+
+    Ham(0,0) = Gamma0;
+    Ham(0,1) = Gamma1;
+    Ham(1,0) = std::conj(Gamma1);
+    Ham(1,1) = Gamma0;
+
+    // Initialize eigenvalue vector and eigenvector matrix and
+    arma::vec eigval;
+    arma::cx_mat eigvecs;
+
+    // Compute eigenvalues
+    bool status = arma::eig_sym(eigval, eigvecs, Ham);
+    if (status == false)
+    {
+        std::cout << "Eigenvalue decomposition failed" << std::endl;
+    }
+
+    // Control loop for positive/negative energy eigenstate
+    arma::cx_vec the_eigenvector;
+
+    if (pos == true)
+    {
+        the_eigenvector = eigvecs.col(0);
+    }
+    else
+    {
+        the_eigenvector = eigvecs.col(1);
+    }
+
+    // Normalisation constant
+    double norm_const
+        = 1.0/(std::norm(the_eigenvector(0)) + std::norm(the_eigenvector(1)));
+
+    // Assign values of the eigenstate
+    state_type eigen = {0.0,0.0,0.0,0.0};
+
+    eigen[0] = norm_const*std::real(the_eigenvector(0));
+    eigen[1] = norm_const*std::imag(the_eigenvector(0));
+    eigen[2] = norm_const*std::real(the_eigenvector(1));
+    eigen[3] = norm_const*std::imag(the_eigenvector(1));
+
+    return eigen;
 }
 
 /// Function computing the projection of two eigenvectors (returns a complex number)
 template <typename T>
 std::complex<T> Projection(std::vector<T> vec1, std::vector<T> vec2)
 {
-        return (vec1[0] - 1i*vec1[1])*(vec2[0] + 1i*vec2[1]) +
-                (vec1[2] - 1i*vec1[3])*(vec2[2] + 1i*vec2[3]);
+    return (vec1[0] - 1i*vec1[1])*(vec2[0] + 1i*vec2[1]) +
+           (vec1[2] - 1i*vec1[3])*(vec2[2] + 1i*vec2[3]);
 }
